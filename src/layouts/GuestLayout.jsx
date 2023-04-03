0e307e0cb6de903d4daaa2d22dadc04f5e695f81
Shockrates <blackhound55@gmail.com>
2023-04-03 12:28:27 +0300

    ProtecteLayout and Register added

--- src/layouts/GuestLayout.jsx
+++ src/layouts/GuestLayout.jsx
@@ -1,7 +1,19 @@
-import React from 'react'
+import React from 'react';
+import { Navigate, Outlet } from 'react-router-dom';
+import { useAuth } from '../contexts/AuthContext';
 
 export default function GuestLayout() {
+
+  const { user } = useAuth;
+
+  // if user is logged in, redirect to profile page
+  if (user) {
+    return <Navigate to="/profile" />;
+  }
+
   return (
-    <div>GuestLayout</div>
+    <>
+      <Outlet />
+    </>
   )
 }
